b5aca4fa62947b4d7d8b4c67675a16fe22730e09
João Pedro <jptiberio@icloud.com>
2017-10-26 12:54:16 -0200

    Initial Commit

---
+++ acf-json-versioning.php
@@ -0,0 +1,43 @@
+<?php
+
+	/**
+	 *
+	 * Plugin Name: ACF Json Versioning
+	 * Description: Plugin que habilita o versionamento de campos do ACF dentro do tema
+	 * Author: JP Tibério
+	 *
+	 */
+
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+	if ( is_plugin_active('advanced-custom-fields-pro/acf.php') ) {
+
+		$theme_root = get_template_directory();
+
+		if(!is_dir($theme_root."/acf-json")){
+			mkdir($theme_root."/acf-json", 0777);
+		}
+
+
+		function acfJsonLoad($paths)
+		{
+		    $paths[] = $theme_root . '/acf-json';
+		    return $paths;
+		}
+
+		add_filter('acf/settings/load_json', 'acfJsonLoad');
+
+	} else {
+
+		function my_error_notice() {
+		    ?>
+		    <div class="error notice">
+		        <p><?php _e( 'Por favor, instale ou ative o ACF Pro!', 'my_plugin_textdomain' ); ?></p>
+		    </div>
+		    <?php
+		}
+		
+		add_action( 'admin_notices', 'my_error_notice' );
+	}
+
+?>
